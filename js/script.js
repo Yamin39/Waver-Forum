0e51e216e1b61935559529ac9b05c1e29d0dbc81
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2024-03-02 23:45:22 +0600

    project initialized

---
+++ js/script.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        "primary-color": "#797DFC",
+        "dark-full": "#12132D",
+        "dark-80": "#12132DCC",
+        "dark-70": "#12132DB3",
+        "dark-60": "#12132D99",
+        "dark-50": "#03071280",
+        "light-80": "#FFFFFFCC",
+      },
+    },
+  },
+};
